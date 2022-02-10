80d3f475328c7eed2ebf6da8997ccb2392e82acc
yirantao <yiran_tao@college.harvard.edu>
2022-02-10 15:49:34 -0500

    question 4

---
+++ scripts/open_space_publisher.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import Float32
+import math
+import operator
+from sensor_msgs.msg import LaserScan
+    
+def callback(data):
+    pub1 = rospy.Publisher('open_space/distance', Float32, queue_size=10)
+    pub2 = rospy.Publisher('open_space/angle', Float32, queue_size=10)
+    rate = rospy.Rate(20) 
+   
+    ranges = data.ranges
+    angle_min = data.angle_min
+    angle_max = data.angle_max
+    angle_increment = data.angle_increment
+    max_index, max_range = max(enumerate(ranges), key=operator.itemgetter(1))
+    max_angle = angle_min + (angle_increment * max_index)
+    #rospy.loginfo(max_index)
+    rospy.loginfo(max_range)
+    pub1.publish(max_range)
+    rospy.loginfo(max_angle)
+    pub2.publish(max_angle)
+    rate.sleep()
+        
+    
+def listener():
+
+    rospy.init_node('open_space_publisher', anonymous=True)
+
+    rospy.Subscriber("fake_scan", LaserScan, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
