ef826216302d7f462a3cf617df691100f0ad7630
yirantao <yiran_tao@college.harvard.edu>
2022-02-10 16:39:34 -0500

    question 5

--- scripts/open_space_publisher.py
+++ scripts/open_space_publisher.py
@@ -4,10 +4,13 @@ from std_msgs.msg import Float32
 import math
 import operator
 from sensor_msgs.msg import LaserScan
+from ros_exercises.msg import OpenSpace
+
     
 def callback(data):
-    pub1 = rospy.Publisher('open_space/distance', Float32, queue_size=10)
-    pub2 = rospy.Publisher('open_space/angle', Float32, queue_size=10)
+    # pub1 = rospy.Publisher('open_space/distance', Float32, queue_size=10)
+    # pub2 = rospy.Publisher('open_space/angle', Float32, queue_size=10)
+    pub = rospy.Publisher('open_space', OpenSpace, queue_size=10)
     rate = rospy.Rate(20) 
    
     ranges = data.ranges
@@ -16,11 +19,16 @@ def callback(data):
     angle_increment = data.angle_increment
     max_index, max_range = max(enumerate(ranges), key=operator.itemgetter(1))
     max_angle = angle_min + (angle_increment * max_index)
-    #rospy.loginfo(max_index)
-    rospy.loginfo(max_range)
-    pub1.publish(max_range)
-    rospy.loginfo(max_angle)
-    pub2.publish(max_angle)
+    
+    ops = OpenSpace()
+    ops.angle = max_angle
+    ops.distance = max_range
+    # rospy.loginfo(max_range)
+    # pub1.publish(max_range)
+    # rospy.loginfo(max_angle)
+    # pub2.publish(max_angle)
+    rospy.loginfo(ops)
+    pub.publish(ops)
     rate.sleep()
         
     
