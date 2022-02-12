0cebd3a6fc1fb094d6501d35a2fd6f6a79eabac1
yirantao <yiran_tao@college.harvard.edu>
2022-02-12 17:54:07 -0500

    question 5

--- scripts/fake_scan_publisher.py
+++ scripts/fake_scan_publisher.py
@@ -7,40 +7,26 @@ from sensor_msgs.msg import LaserScan
 import math
 
  
-def publisher():    
-    rospy.init_node('fake_scan_publisher')   
-    
-    publish_topic = rospy.get_param('publish topic', 'fake_scan')   
-    publish_rate = rospy.get_param('publish rate', 20)
-    angle_min = rospy.get_param('angle_min', -(2/3)*math.pi)
-    angle_max = rospy.get_param('angle_max', (2/3)*math.pi)
-    range_min = rospy.get_param('range_min', 1.0)
-    range_max = rospy.get_param('range_max', 10.0)
-    angle_increment = rospy.get_param('angle_increment', (1/300)*math.pi)
-    
-    pub = rospy.Publisher(publish_topic, LaserScan, queue_size=10)
-    rate = rospy.Rate(publish_rate) 
-    
-    
+def publisher():
+    pub = rospy.Publisher('fake_scan', LaserScan, queue_size=10)
+    rospy.init_node('fake_scan_publisher', anonymous=True)
+    rate = rospy.Rate(20) 
     while not rospy.is_shutdown():
         ls = LaserScan()
         ls.header.stamp = rospy.Time.now()
         ls.header.frame_id = 'base_link'
-        ls.angle_min = angle_min
-        ls.angle_max = angle_max
-        ls.angle_increment = angle_increment
+        ls.angle_min = -(2/3)*math.pi
+        ls.angle_max = (2/3)*math.pi
+        ls.angle_increment = (1/300)*math.pi
         ls.scan_time = 1/20
-        ls.range_min = range_min
-        ls.range_max = range_max
+        ls.range_min = 1.0
+        ls.range_max = 10.0
         #length 401
         ranges = []
-        length = (ls.angle_max - ls.angle_min)/ls.angle_increment+1
-        length = int(length+0.1)
-        #rospy.loginfo(rospy.has_param('publish_topic'))
-        for i in range(length):            
-            random_number = random.uniform(ls.range_min,ls.range_max)
+        for i in range(401):            
+            random_number = random.uniform(1.0,10.0)
             ranges.append(random_number)
-        assert len(ranges)==length
+        assert len(ranges)==401
         ls.ranges = ranges
         pub.publish(ls)
         rate.sleep()
